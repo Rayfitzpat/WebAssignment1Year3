84878c4e2df1b9a7164d6075489e682c3fe550ae
ray_f <ray_fitzpatrick@hotmail.com>
2021-10-18 15:51:23 +0100

    first commit

---
+++ database.php
@@ -0,0 +1,17 @@
+<?php
+        $dsn = 'mysql:host=localhost;port=3306;dbname=filmsdb';
+        $username = 'root';
+        $password = '';
+
+        try {
+            // success
+            $db = new PDO($dsn, $username, $password);
+            //echo '<p>You are connected to the database!</p>';
+        } catch (PDOException $e) {
+            // error
+            $error_message = $e->getMessage();          
+            //echo "<p>An error occurred while connecting to  the database: $error_message </p>";
+            include('database_error.php');
+            exit();
+        }
+    ?>
---
+++ databaseError.php
@@ -0,0 +1,5 @@
+<h1>Database Error</h1>
+<p>There was an error connecting to the database.</p>
+<p>Check that the database is created and the server is running.</p>
+<p>Error message: <?php echo $error_message; ?></p>
+<p>&nbsp;</p>
